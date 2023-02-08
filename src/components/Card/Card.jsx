0988f9f398a942322c2a2aa0102d7c712f77a755
Becky Rushton <beckyrushton30@gmail.com>
2023-02-08 12:36:56 +0000

    added functioning plus and minus buttons to ticket tracker

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import "./Card.scss";
+import { useState } from "react";
 
 const Card = (props) => {
   const { name, role, ticketAmount } = props;
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter === 0) {
+      return 0;
+    } else {
+      setCounter(counter - 1);
+    }
+  };
   return (
     <div className="card">
       <h2>Name: {name}</h2>
       <h2>Role: {role}</h2>
       <div className="counter">
-        <button className="minus">-</button>
-        <p>{ticketAmount}</p>
-        <button className="plus">+</button>
+        <div onClick={handleDecrement} className="minus">
+          <button className="plus">-</button>
+        </div>
+        <p className="ticketNum">{counter}</p>
+        <div onClick={handleIncrement} className="plus">
+          <button className="plus">+</button>
+        </div>
       </div>
     </div>
   );
